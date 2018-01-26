178686fb5e608eb37f5df1190a9eef45230d0b9f
parkhabit <parkhabit@gmail.com>
2018-01-26 10:18:27 +0000

    changed contact screen

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const navOverlay = document.getElementById("full_nav");
 const navOpenButton = document.getElementById("nav");
 const navCloseButton = document.getElementById("closebtn");
+const about_link = document.getElementById("about__link");
+const project_link = document.getElementById("project__link");
+const contact_link = document.getElementById("contact__link");
 
 navOpenButton.addEventListener("click", function() {
   navOverlay.classList.add("overlay__visible");
 });
 
 navCloseButton.addEventListener("click", function() {
+  console.log("hello i am working");
+  navOverlay.classList.remove("overlay__visible");
+});
+
+about_link.addEventListener("click", function() {
+  navOverlay.classList.remove("overlay__visible");
+});
+
+project_link.addEventListener("click", function() {
+  navOverlay.classList.remove("overlay__visible");
+});
+
+contact_link.addEventListener("click", function() {
   navOverlay.classList.remove("overlay__visible");
 });
